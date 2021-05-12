5ce25e418e5a41136de986d3f97b541756da2477
Joon Kang <joonkang@Joons-MacBook-Pro.local>
2021-05-12 14:26:51 +0900

    update

--- jumpstart-latest-ground/split_ground_data.py
+++ jumpstart-latest-ground/split_ground_data.py
@@ -13,22 +13,25 @@ def split_data(file_path):
     try:
         with open(file_path) as f:
             content = f.read()
-    try: #try parsing to dict
-        dataform = str(response_json).strip("'<>() ").replace('\'', '\"')
-        new_dict = json.loads(content)
-        terminal_data = new_dict.get('ns2:TATrackAndFlightPlan')
-        if terminal_data:
-            insert_flight(new_dict)
-        else:
-            other = new_dict.get('ns2:asdexMsg')
-            if other:
-                surface_data = new_dict['ns2:asdexMsg'].get('xmlns:ns2')
-                if surface_data:
-                    if new_dict['ns2:asdexMsg']['xmlns:ns2'] == "urn:us:gov:dot:faa:atm:terminal:entities:v4-0:smes:surfacemovementevent":
-                        insert_ground(new_dict)
-        f.close()
-        os.remove(file_path)
+        try: #try parsing to dict
+            dataform = str(response_json).strip("'<>() ").replace('\'', '\"')
+            new_dict = json.loads(content)
+            terminal_data = new_dict.get('ns2:TATrackAndFlightPlan')
+            if terminal_data:
+                insert_flight(new_dict)
+            else:
+                other = new_dict.get('ns2:asdexMsg')
+                if other:
+                    surface_data = new_dict['ns2:asdexMsg'].get('xmlns:ns2')
+                    if surface_data:
+                        if new_dict['ns2:asdexMsg']['xmlns:ns2'] == "urn:us:gov:dot:faa:atm:terminal:entities:v4-0:smes:surfacemovementevent":
+                            insert_ground(new_dict)
+            f.close()
+            os.remove(file_path)
 
-    except:
-        print repr(resonse_json)
-        print sys.exc_info()
+        except:
+            print repr(resonse_json)
+            print sys.exc_info()
+
+    except IOError:
+        pass
